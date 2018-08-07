d0d6ce79f420f0f7d06d8ce58d13b84319432428
github username <github email address>
2018-08-07 13:56:51 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,25 @@
-## code your solution here. 
+class Cat
+  # def name=(cats_name)
+  #     @name = cats_name
+  # end
+  #
+  # def name
+  #   @name
+  # end
+# above code is same as "attr_accessor :name"
+  attr_accessor :name
+
+  def meow
+    puts "meow!"
+  end
+end
+
+maru = Cat.new
+maru.name = "Maru"
+
+maru.name
+# => "Maru"
+
+maru.meow
+# "meow!"
+# => nil
